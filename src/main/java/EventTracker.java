08fd89294b88cbc65c44e5b198c27f1af2b8fc1f
Jacob C <jacobc@Zipcoders-MacBook-Pro.local>
2023-04-12 19:25:32 -0400

    done

--- src/main/java/EventTracker.java
+++ src/main/java/EventTracker.java
@@ -16,15 +16,30 @@ public class EventTracker implements Tracker {
     }
 
     synchronized public void push(String message) {
+        if (has(message)) {
+            tracker.put(message, tracker.get(message) + 1);
+        } else {
+            tracker.put(message, 1);
+        }
     }
 
     synchronized public Boolean has(String message) {
-        return null;
-    }
+        if (tracker.containsKey(message)){
+            return tracker.get(message) > 0;
+        }
+    return false;
+}
 
     synchronized public void handle(String message, EventHandler e) {
+        e.handle();
+        if (has(message)){
+            tracker.put(message, tracker.get(message) - 1);
+        }
     }
-
+    @Override
+    public Map<String, Integer> tracker(){
+        return null;
+    }
     // Do not use this. This constructor is for tests only
     // Using it breaks the singleton class
     EventTracker(Map<String, Integer> tracker) {
